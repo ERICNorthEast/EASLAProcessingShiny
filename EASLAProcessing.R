77657947c533839103f7b4d9fb946dc92cd4b575
ERIC North East <eric.ne@twmuseums.org.uk>
2022-06-01 09:21:52 +0100

    Tidy code

--- EASLAProcessing.R
+++ EASLAProcessing.R
@@ -32,26 +32,16 @@ ui <- fluidPage(
 
 # minimal Shiny server
 server <- function(input, output) {
-
-
   output$okBtn <- downloadHandler(
     filename = function() {
-      ifelse(stringr::str_ends(input$outputfile,'xlsx'),input$outputfile,paste0(input$outputfile,'.xlsx'))
+      ifelse(
+        stringr::str_ends(input$outputfile, 'xlsx'),
+        input$outputfile,
+        paste0(input$outputfile, '.xlsx')
+      )
 
     },
     content = function(file) {
-
-
-      # inFile <- input$csvfile
-      #
-      # if (is.null(inFile))
-      #   return(NULL)
-      #
-      # ea_data <- read.csv(inFile$datapath,header = TRUE)
-
-
-      started <- Sys.time()
-
       #Create output workbook
       XL_wb <- openxlsx::createWorkbook()
 
@@ -65,51 +55,29 @@ server <- function(input, output) {
       locationCol <- EA_SLA_config["locationCol"]
       abundanceCol <- EA_SLA_config["abundanceCol"]
       commentCol <- EA_SLA_config["commentCol"]
-      recorderCol <- EA_SLA_config["recorderCol"]
       lastCol <- EA_SLA_config["lastCol"]
 
-      ea_data <- read.csv(inFile$datapath,header = TRUE)
+      ea_data <- read.csv(inFile$datapath, header = TRUE)
 
 
-        #Format the date - two fields
-        ea_data$Survey_sta <- formatDates(ea_data$Survey_sta)
-        ea_data$Survey_end <- formatDates(ea_data$Survey_end)
-
-        #Filter out the records we don't want
-        #ea_data <- filter_by_survey(ea_data)
-
-        #Remove GCN licence zero counts
-        #ea_data <- filter_GCN(ea_data)
-
-        #Fish
-        #ea_data <- do_data_fixes(ea_data)
-
-        #Set protected flag
-        # #Not sure prot species is correct - = not "in"
-
-        #ea_data <- set_protected_flag(ea_data)
+      #Format the date - two fields
+      ea_data$Survey_sta <- formatDates(ea_data$Survey_sta)
+      ea_data$Survey_end <- formatDates(ea_data$Survey_end)
 
 
-        # #Sort out designations
-        #ea_data <- fix_designations(ea_data,TRUE)
+      #Get the columns we're going to output
+      outputdata <-
+        format_and_check_EA_SLA_data(ea_data, OutputCols, newColNames)
 
-        # Easting & northing calculations
-        #ea_data <- E_and_N_calcs(ea_data)
-
-
-        #Get the columns we're going to output
-        #AED this needs checking
-        browser()
-        outputdata <- format_and_check_EA_SLA_data(ea_data,OutputCols,newColNames)
-
-        sheet_name = 'SLA data'
-        XL_wb <- openxlsx::createWorkbook()
+      sheet_name = 'EA SLA data'
+      XL_wb <- openxlsx::createWorkbook()
 
 
-        XL_wb <- format_EA_SLA_Excel_output(XL_wb,sheet_name,outputdata,EA_SLA_config)
+      XL_wb <-
+        format_EA_SLA_Excel_output(XL_wb, sheet_name, outputdata, EA_SLA_config)
 
 
-        openxlsx::saveWorkbook(XL_wb,file,overwrite = TRUE)
+      openxlsx::saveWorkbook(XL_wb, file, overwrite = TRUE)
     }
   )
 
